23c6a172ab1546d9351ccba6c44285a4859acb21
Nichole McGrew <nicholemcgrew@gmail.com>
2024-04-09 07:23:54 -0500

    react dashboard

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Header from '../src/components/Header'
+import Sidebar from '../src/components/Sidebar'
+import Home from '../src/components/Home'
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
+}
+
+export default App
